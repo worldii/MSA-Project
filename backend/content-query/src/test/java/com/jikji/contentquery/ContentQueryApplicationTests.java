58fa59d442f81ad422fa32c1df40c6214034b4c0
mardi2020 <mardi@khu.ac.kr>
2023-01-19 10:51:06 +0900

    feat: content 서비스 생성

---
+++ backend/content-query/src/test/java/com/jikji/contentquery/ContentQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikji.contentquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
