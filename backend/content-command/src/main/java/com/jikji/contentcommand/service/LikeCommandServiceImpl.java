cd6bd998a0a3b2a380bdf0f1c72defab0cca1d12
mardi2020 <mardi@khu.ac.kr>
2023-02-01 18:50:08 +0900

    feat: kafka 추가

--- backend/content-command/src/main/java/com/jikji/contentcommand/service/LikeCommandServiceImpl.java
+++ backend/content-command/src/main/java/com/jikji/contentcommand/service/LikeCommandServiceImpl.java
@@ -1,14 +1,20 @@
 package com.jikji.contentcommand.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.jikji.contentcommand.domain.Content;
 import com.jikji.contentcommand.domain.ContentLike;
+import com.jikji.contentcommand.dto.message.LikeKafkaMessage;
 import com.jikji.contentcommand.exception.ContentNotFoundException;
 import com.jikji.contentcommand.exception.LikeNotFoundException;
 import com.jikji.contentcommand.repository.ContentCommandRepository;
 import com.jikji.contentcommand.repository.LikeCommandRepository;
+import com.jikji.contentcommand.util.KafkaTopic;
 import com.jikji.contentcommand.util.ValidCheckUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +25,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class LikeCommandServiceImpl implements LikeCommandService {
 
     private final LikeCommandRepository likeCommandRepository;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     private final ContentCommandRepository contentCommandRepository;
 
@@ -35,7 +43,7 @@ public class LikeCommandServiceImpl implements LikeCommandService {
                 .save(new ContentLike(userId, contentId));
 
         likeCommandRepository.save(contentLike);
-
+        sendMessage(contentLike, KafkaTopic.ADD_CONTENT_LIKE);
         return content.getLikes();
     }
 
@@ -49,7 +57,25 @@ public class LikeCommandServiceImpl implements LikeCommandService {
         ContentLike contentLike = likeCommandRepository.findByUserIdAndContentId(userId, contentId)
                 .orElseThrow(LikeNotFoundException::new);
         likeCommandRepository.delete(contentLike);
-
+        sendMessage(contentLike, KafkaTopic.DELETE_CONTENT_LIKE);
         return content.getLikes();
+    }
+
+    private void sendMessage(ContentLike contentLike, String topic) {
+        ObjectWriter writer = new ObjectMapper().writer();
+        String data = null;
+        try {
+            LikeKafkaMessage message = LikeKafkaMessage.builder()
+                    .contentId(contentLike.getContentId())
+                    .userId(contentLike.getUserId())
+                    .contentLikeId(contentLike.getContentId())
+                    .build();
+
+            data = writer.writeValueAsString(message);
+            kafkaTemplate.send(topic, data);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+        }
+        log.info("content kafka send - " + data);
     }
 }
