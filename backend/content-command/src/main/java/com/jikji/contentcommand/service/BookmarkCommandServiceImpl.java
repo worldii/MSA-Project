d3512fff24031b90c4bfc75397de606aea287a39
mardi2020 <mardi@khu.ac.kr>
2023-02-01 19:52:44 +0900

    feat: kafka 추가

--- backend/content-command/src/main/java/com/jikji/contentcommand/service/BookmarkCommandServiceImpl.java
+++ backend/content-command/src/main/java/com/jikji/contentcommand/service/BookmarkCommandServiceImpl.java
@@ -1,14 +1,20 @@
 package com.jikji.contentcommand.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.jikji.contentcommand.domain.Bookmark;
 import com.jikji.contentcommand.domain.Content;
+import com.jikji.contentcommand.dto.message.BookmarkKafkaMessage;
 import com.jikji.contentcommand.exception.BookmarkNotFoundException;
 import com.jikji.contentcommand.exception.ContentNotFoundException;
 import com.jikji.contentcommand.repository.BookmarkCommandRepository;
 import com.jikji.contentcommand.repository.ContentCommandRepository;
+import com.jikji.contentcommand.util.KafkaTopic;
 import com.jikji.contentcommand.util.ValidCheckUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +25,8 @@ public class BookmarkCommandServiceImpl implements BookmarkCommandService {
     private final BookmarkCommandRepository bookmarkCommandRepository;
 
     private final ContentCommandRepository contentCommandRepository;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
     public Long saveBookmark(Long userId, Long contentId) {
@@ -33,6 +41,7 @@ public class BookmarkCommandServiceImpl implements BookmarkCommandService {
                         .userId(userId)
                         .build());
 
+        sendMessage(savedBookmark, 0, content.getLikes());
         return savedBookmark.getId();
     }
 
@@ -42,5 +51,24 @@ public class BookmarkCommandServiceImpl implements BookmarkCommandService {
                 .orElseThrow(BookmarkNotFoundException::new);
 
         bookmarkCommandRepository.delete(bookmark);
+        kafkaTemplate.send(KafkaTopic.DELETE_BOOKMARK, String.valueOf(bookmark.getId()));
+    }
+
+    private void sendMessage(Bookmark bookmark, int commentCount, int likesCount) {
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String data = null;
+        try {
+            data = writer.writeValueAsString(BookmarkKafkaMessage.builder()
+                    .bookmarkId(bookmark.getId())
+                    .userId(bookmark.getUserId())
+                    .contentId(bookmark.getContentId())
+                    .commentCount(commentCount)
+                    .likesCount(likesCount));
+
+            kafkaTemplate.send(KafkaTopic.ADD_BOOKMARK, data);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+        }
+        log.info("content kafka send - " + data);
     }
 }
