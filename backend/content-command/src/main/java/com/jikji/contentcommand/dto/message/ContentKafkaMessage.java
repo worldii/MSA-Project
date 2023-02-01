f27af1b7a407c650f7e04264e321765d63785d45
mardi2020 <mardi@khu.ac.kr>
2023-02-01 16:02:54 +0900

    feat: kafka 토픽, send 추가

---
+++ backend/content-command/src/main/java/com/jikji/contentcommand/dto/message/ContentKafkaMessage.java
@@ -0,0 +1,40 @@
+package com.jikji.contentcommand.dto.message;
+
+import com.jikji.contentcommand.domain.Content;
+import com.jikji.contentcommand.domain.ImageUrl;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ContentKafkaMessage {
+
+    private Long contentId;
+    private Long userId;
+    private String text;
+    private List<ImageUrl> imageUrl;
+
+    private int likes;
+
+    private Boolean visibleLikes;
+
+    private Boolean visibleComments;
+
+    private String createdAt;
+
+    private String modifiedAt;
+
+    public ContentKafkaMessage(Content content) {
+        this.contentId = content.getId();
+        this.userId = content.getUserId();
+        this.text = content.getText();
+        this.imageUrl = content.getImageUrl();
+        this.createdAt = content.getCreatedAt();
+        this.modifiedAt = content.getModifiedAt();
+        this.likes = content.getLikes();
+        this.visibleComments = content.getVisibleComments();
+        this.visibleLikes = content.getVisibleLikes();
+    }
+}
--- backend/content-command/src/main/java/com/jikji/contentcommand/service/ContentCommandServiceImpl.java
+++ backend/content-command/src/main/java/com/jikji/contentcommand/service/ContentCommandServiceImpl.java
@@ -1,14 +1,20 @@
 package com.jikji.contentcommand.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.jikji.contentcommand.domain.Content;
+import com.jikji.contentcommand.dto.message.ContentKafkaMessage;
 import com.jikji.contentcommand.dto.request.ContentCreateRequest;
 import com.jikji.contentcommand.dto.request.ContentUpdateRequest;
 import com.jikji.contentcommand.exception.ContentNotFoundException;
 import com.jikji.contentcommand.repository.ContentCommandRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import util.KafkaTopic;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +24,12 @@ public class ContentCommandServiceImpl implements ContentCommandService {
 
     private final ContentCommandRepository contentCommandRepository;
 
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Override
     public Long save(final ContentCreateRequest request) {
         Content savedContent = contentCommandRepository.save(request.toEntity());
+        sendMessage(savedContent, KafkaTopic.ADD_CONTENT);
         return savedContent.getId();
     }
 
@@ -29,18 +38,20 @@ public class ContentCommandServiceImpl implements ContentCommandService {
         final Content content = contentCommandRepository.findById(contentId)
                 .orElseThrow(ContentNotFoundException::new);
         content.update(request);
+        sendMessage(content, KafkaTopic.UPDATE_CONTENT);
     }
 
     @Override
     public void delete(Long contentId) {
         contentCommandRepository.deleteById(contentId);
+        kafkaTemplate.send(KafkaTopic.DELETE_CONTENT, String.valueOf(contentId));
     }
 
     public boolean visibilityLikes(Long contentId) {
         Content content = getContent(contentId);
         content.changeVisibleLikes();
         Content savedContent = contentCommandRepository.save(content);
-
+        kafkaTemplate.send(KafkaTopic.VISIBLE_LIKES, String.valueOf(contentId));
         return savedContent.getVisibleLikes();
     }
 
@@ -48,12 +59,25 @@ public class ContentCommandServiceImpl implements ContentCommandService {
         Content content = getContent(contentId);
         content.changeVisibleComments();
         Content savedContent = contentCommandRepository.save(content);
-
+        kafkaTemplate.send(KafkaTopic.VISIBLE_COMMENT, String.valueOf(contentId));
         return savedContent.getVisibleComments();
     }
 
     private Content getContent(Long contentId) {
         return contentCommandRepository.findById(contentId)
                 .orElseThrow(ContentNotFoundException::new);
+    }
+
+    private void sendMessage(Content content, String topic) {
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String data = null;
+        try {
+            ContentKafkaMessage message = new ContentKafkaMessage(content);
+            data = writer.writeValueAsString(message);
+            kafkaTemplate.send(topic, data);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+        }
+        log.info("content kafka send - " + data);
     }
 }
