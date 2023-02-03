6fc4578884aaf2d8353831e595bb6db71ce41ae8
mardi2020 <mardi@khu.ac.kr>
2023-02-03 17:38:55 +0900

    feat: kafka topic hashtag 추가

--- backend/content-query/src/main/java/com/jikji/contentquery/util/KafkaTopic.java
+++ backend/content-query/src/main/java/com/jikji/contentquery/util/KafkaTopic.java
@@ -14,4 +14,6 @@ public final class KafkaTopic {
 
     public static final String ADD_CONTENT_LIKE = "like-add";
     public static final String DELETE_CONTENT_LIKE = "like-delete";
+
+    public static final String UPDATE_HASHTAG = "hashtag-update";
 }
