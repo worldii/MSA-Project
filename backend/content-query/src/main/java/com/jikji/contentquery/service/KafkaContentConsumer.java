30ade6d22d9fbc4fc4832804de065a249d9c9480
mardi2020 <mardi@khu.ac.kr>
2023-02-03 17:39:15 +0900

    feat: kafka consumer 추가

--- backend/content-query/src/main/java/com/jikji/contentquery/service/KafkaContentConsumer.java
+++ backend/content-query/src/main/java/com/jikji/contentquery/service/KafkaContentConsumer.java
@@ -4,6 +4,7 @@ package com.jikji.contentquery.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jikji.contentquery.domain.Content;
+import com.jikji.contentquery.dto.message.HashtagKafkaMessage;
 import com.jikji.contentquery.exception.ContentNotFoundException;
 import com.jikji.contentquery.repository.ContentQueryRepository;
 import com.jikji.contentquery.util.KafkaTopic;
@@ -23,13 +24,13 @@ public class KafkaContentConsumer {
 
     @KafkaListener(topics = KafkaTopic.ADD_CONTENT)
     public void addContent(String message) throws JsonProcessingException {
-        Content content = readValueByJson(message);
+        Content content = readContentByJson(message);
         contentQueryRepository.save(content);
     }
 
     @KafkaListener(topics = KafkaTopic.UPDATE_CONTENT)
     public void updateContent(String message) throws JsonProcessingException {
-        Content content = readValueByJson(message);
+        Content content = readContentByJson(message);
         Content storedContent = getContentById(content.getContentId());
         storedContent.updateContent(content);
         contentQueryRepository.save(storedContent);
@@ -43,12 +44,39 @@ public class KafkaContentConsumer {
         contentQueryRepository.delete(storedContent);
     }
 
+    @KafkaListener(topics = KafkaTopic.UPDATE_HASHTAG)
+    public void updateHashtag(String message) throws JsonProcessingException {
+        HashtagKafkaMessage hashtags = mapper.readValue(message, HashtagKafkaMessage.class);
+        Long contentId = hashtags.getContentId();
+
+        Content content = getContentById(contentId);
+
+        content.updateHashtags(hashtags.getHashtags());
+        contentQueryRepository.save(content);
+    }
+
+    @KafkaListener(topics = KafkaTopic.VISIBLE_LIKES)
+    public void visibilityLikes(String message) throws JsonProcessingException {
+        Long contentId = mapper.readValue(message, Long.class);
+        Content content = getContentById(contentId);
+        content.updateVisibilityLikes();
+        contentQueryRepository.save(content);
+    }
+
+    @KafkaListener(topics = KafkaTopic.VISIBLE_COMMENT)
+    public void visibilityComments(String message) throws JsonProcessingException {
+        Long contentId = mapper.readValue(message, Long.class);
+        Content content = getContentById(contentId);
+        content.updateVisibilityComments();
+        contentQueryRepository.save(content);
+    }
+
     private Content getContentById(Long id) {
         return contentQueryRepository.findByContentId(id)
                 .orElseThrow(ContentNotFoundException::new);
     }
 
-    private Content readValueByJson(String json) throws JsonProcessingException {
+    private Content readContentByJson(String json) throws JsonProcessingException {
         log.info("[Kafka message]: " + json);
         return mapper.readValue(json, Content.class);
     }
