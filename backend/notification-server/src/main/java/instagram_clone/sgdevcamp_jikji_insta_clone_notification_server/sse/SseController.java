eddff169fcf19cf4144cbde0f545a3357b2fded5
0Chord <seaweed.0chord@gmail.com>
2023-02-08 21:43:14 +0900

    feat: 알림서버 초기버전

--- backend/notification-server/src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_notification_server/sse/SseController.java
+++ backend/notification-server/src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_notification_server/sse/SseController.java
@@ -1,4 +1,4 @@
-package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.sse;
+package instagram_clone.sgdevcamp_jikji_insta_clone_notification_server.sse;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,36 +11,33 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.service.JwtService;
-import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.User;
-import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequestMapping("/notification")
 public class SseController {
 
 	public static Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
-	private final JwtService jwtService;
-	private final NotificationService notificationService;
-	private final UserService userService;
 
-	public SseController(JwtService jwtService, NotificationService notificationService, UserService userService) {
-		this.jwtService = jwtService;
+	private final NotificationService notificationService;
+
+	public SseController(NotificationService notificationService) {
 		this.notificationService = notificationService;
-		this.userService = userService;
 	}
 
-	@CrossOrigin
-	@GetMapping(value = "/sub", consumes = MediaType.ALL_VALUE)
+	@CrossOrigin("*")
+	@GetMapping(value = "/subscribe", consumes = MediaType.ALL_VALUE)
 	public SseEmitter subscribe(@RequestParam String token,
 		@RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "")
 		String lastEventId) {
-		String email = jwtService.getEmail(token);
+		String id = token + "_" + System.currentTimeMillis();
+		System.out.println("email = " + token);
 		SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
 		try {
 			emitter.send(SseEmitter.event().name("connect"));
@@ -48,21 +45,19 @@ public class SseController {
 			e.printStackTrace();
 		}
 
-		sseEmitters.put(email, emitter);
-		System.out.println("sseEmitters = " + sseEmitters);
-		emitter.onCompletion(() -> sseEmitters.remove(email));
-		emitter.onTimeout(() -> sseEmitters.remove(email));
-		emitter.onError((e) -> sseEmitters.remove(email));
+		notificationService.save(id,emitter);
+		emitter.onCompletion(() -> sseEmitters.remove(token));
+		emitter.onTimeout(() -> sseEmitters.remove(token));
+		emitter.onError((e) -> sseEmitters.remove(token));
 
 		return emitter;
 	}
 
+
 	@PostMapping("/add")
 	public ResponseEntity<?> add() {
-		List<User> all = userService.findAll();
-		for (User user : all) {
-			notificationService.noticeAllMember(user.getEmail());
-		}
+
+
 		return ResponseEntity.ok("ok");
 	}
 
---
+++ backend/notification-server/src/test/java/instagram_clone/sgdevcamp_jikji_insta_clone_notification_server/SgdevcampJikjiInstaCloneNotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram_clone.sgdevcamp_jikji_insta_clone_notification_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdevcampJikjiInstaCloneNotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
