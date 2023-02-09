c889e772f5787c9c2698048b21b148dbd83b83e1
0Chord <seaweed.0chord@gmail.com>
2023-02-09 21:29:41 +0900

    feat: 알림서버 서비스 구체화

---
+++ backend/notification-server/src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_notification_server/sse/util/LocalDateTimeToArray.java
@@ -0,0 +1,21 @@
+package instagram_clone.sgdevcamp_jikji_insta_clone_notification_server.sse.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+public class LocalDateTimeToArray {
+	private LocalDateTimeToArray() {
+	}
+
+	public static Integer[] convert(LocalDateTime localDateTime) {
+		if (localDateTime == null) {
+			return new Integer[3];
+		}
+		String dateString = localDateTime.format(DateTimeFormatter.ISO_DATE);
+
+		return Arrays.stream(dateString.split("-"))
+			.map(Integer::valueOf)
+			.toArray(Integer[]::new);
+	}
+}
