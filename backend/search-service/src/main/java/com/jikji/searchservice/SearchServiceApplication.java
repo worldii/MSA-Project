708bb763a07b5a70c93aa4a5748a1eea3c3c518e
mardi2020 <mardi@khu.ac.kr>
2023-02-15 21:06:18 +0900

    chore: 검색 서버 프로젝트 생성

---
+++ backend/search-service/src/main/java/com/jikji/searchservice/SearchServiceApplication.java
@@ -0,0 +1,19 @@
+package com.jikji.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
+@EnableJpaRepositories
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
