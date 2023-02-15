950e0c5d19b88239df01c4af0e9dcfdcc8320c4e
mardi2020 <mardi@khu.ac.kr>
2023-02-15 21:22:57 +0900

    feat: content 기능 추가

---
+++ backend/search-service/src/main/java/com/example/searchservice/repository/ContentRepository.java
@@ -0,0 +1,17 @@
+package com.example.searchservice.repository;
+
+import com.example.searchservice.domain.ContentIndex;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.CountQuery;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ContentRepository extends ElasticsearchRepository<ContentIndex, Long> {
+
+    @Query(value = "{\"bool\": {\"must\": [{\"term\": {\"hashtags\": \"?0\"}}]}}")
+    List<ContentIndex> findAllByHashtagUsingQuery(String hashtag, Pageable pageable);
+
+    @CountQuery(value = "{\"term\": {\"hashtags\": \"?0\"}}")
+    Long countByHashtagsUsingQuery(String hashtag);
+}
