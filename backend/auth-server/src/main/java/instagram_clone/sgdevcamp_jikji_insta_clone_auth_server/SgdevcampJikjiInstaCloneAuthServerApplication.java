ddcc08d703f014dc537d78f8962927b6dabedb72
mardi2020 <mardi@khu.ac.kr>
2023-02-20 15:06:17 +0900

    chore: gateway 라우팅 정보 등록

--- backend/auth-server/src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/SgdevcampJikjiInstaCloneAuthServerApplication.java
+++ backend/auth-server/src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/SgdevcampJikjiInstaCloneAuthServerApplication.java
@@ -2,8 +2,10 @@ package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SgdevcampJikjiInstaCloneAuthServerApplication {
 
 	public static void main(String[] args) {
