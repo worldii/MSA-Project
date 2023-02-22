57fa80b176c578cc6a1e4ce4eb82ee6313d60e4c
0Chord <seaweed.0chord@gmail.com>
2023-02-22 22:15:34 +0900

    feat: 추가 frontend /user-service/user/get-pk 연결

--- frontend/src/components/action/loginUser.js
+++ frontend/src/components/action/loginUser.js
@@ -1,10 +1,13 @@
 import axios from "axios";
-
+import EventSourceObject from "../instance/EventSource";
+import header from "../instance/Header";
 const clearFunc = () => {
     window.location.reload();
 }
 
+
 export async function loginUser(data, setEmailError, setPasswordError, history) {
+    const eventSource = new EventSourceObject();
     await axios.post('/user-service/user/login', data)
         .then(response => {
                 console.log(response.data)
@@ -12,8 +15,10 @@ export async function loginUser(data, setEmailError, setPasswordError, history) 
                 setPasswordError("")
                 if (response.data === "NotFoundUser") {
                     setEmailError("사용자를 찾을 수 없습니다.");
+                    history("/")
                 } else if (response.data === "WrongPassword") {
                     setPasswordError("비밀번호가 일치하지 않습니다.");
+                    history("/")
                 } else if (response.data === "NotEmailAuthUser") {
                     history("/signup/mailAuth",{state:{email:data.email}})
                 }
@@ -21,6 +26,13 @@ export async function loginUser(data, setEmailError, setPasswordError, history) 
                 const refreshToken = response.data["refreshToken"];
                 localStorage.setItem("accessToken", accessToken);
                 localStorage.setItem("refreshToken", refreshToken);
+                axios.get("/user-service/accessToken/get-pk",{
+                    headers: header(accessToken)
+                })
+                    .then(res=>{
+                        eventSource.subscribe(res.data);
+                    })
+                    .catch(err=>console.log(err))
 
                 clearFunc();
             }
